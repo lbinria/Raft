3b8737b047178412bd5d2f6e3b81cf77bdbb94c6
lbinria <benjamin.loillier@inria.fr>
2023-06-26 16:45:33 +0200

    Clean up.

--- src/main/java/org/lbee/models/CandidateState.java
+++ src/main/java/org/lbee/models/CandidateState.java
@@ -7,24 +7,17 @@ public class CandidateState {
 
     private final ArrayList<String> responded;
     private final ArrayList<String> granted;
-    private final HashMap<String, ArrayList<Entry>> logs;
 
     public CandidateState() {
         this.responded = new ArrayList<>();
         this.granted = new ArrayList<>();
-        this.logs = new HashMap<>();
     }
 
     public void clear() {
         this.responded.clear();
         this.granted.clear();
-        this.logs.clear();
     }
 
     public ArrayList<String> getResponded() { return responded; }
     public ArrayList<String> getGranted() { return granted; }
-
-    public HashMap<String, ArrayList<Entry>> getLogs() {
-        return logs;
-    }
 }
